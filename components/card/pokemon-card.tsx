415e8daab55f5b04243df2afe086056c30bc5782
Jaydeep <jaydeep@benzatine.com>
2025-02-25 12:15:57 +0530

    refactor

--- components/card/pokemon-card.tsx
+++ components/card/pokemon-card.tsx
@@ -6,8 +6,8 @@ export default function PokemonCard({ pokemon }: { pokemon: IPokemon }) {
       id="card"
       className="bg-slate-200 h-72 w-full max-w-72 rounded-md p-2 m-2 text-black"
     >
-      <img src={pokemon.photoUrl} className='w-full' alt={pokemon.name} />
-      <p className='text-center'>{pokemon.name}</p>
+      <img src={pokemon.photoUrl} className="w-full" alt={pokemon.name} />
+      <p className="text-center">{pokemon.name}</p>
     </div>
   );
 }
